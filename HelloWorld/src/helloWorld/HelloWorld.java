4d7e6fa69ea9d2ec01ad782b2816ee42c07552eb
FraStudy <Study@LAPTOP-8RCKDC60>
2018-04-12 16:12:41 +0200

    new programm

---
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int [] anz = {1, 3, 5};
+		System.out.println("Hello");
+		
+		for(int i=0; i<anz.length; i++) {
+			System.out.println("Zahl: "+anz[i]);
+		}
+
+	}
+
+}
