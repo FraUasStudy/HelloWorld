31d067505760c303a18e67c23986901a387975b3
FraStudy <Study@LAPTOP-8RCKDC60>
2018-04-12 16:21:49 +0200

    add a sum of the values

--- HelloWorld/src/helloWorld/HelloWorld.java
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -4,12 +4,13 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		int [] anz = {1, 3, 5};
+		int sum = 0;
 		System.out.println("Hello");
 		
 		for(int i=0; i<anz.length; i++) {
 			System.out.println("Zahl: "+anz[i]);
-		}
 
+		}
 	}
 
 }
